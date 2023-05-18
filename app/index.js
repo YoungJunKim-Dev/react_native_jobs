0301af692131a079c1e00cd3b8457ac27aa7b8bf
YJK <youngjunkim.dev@gmail.com>
2023-05-18 17:45:44 +0900

    react_native_jobs init

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+//View = div
+//Text = p
+
+const Home = () => {
+  return (
+    <View>
+      <Text>This is Home</Text>
+    </View>
+  );
+};
+
+export default Home;
